5887d03a33a3776dd129892261725a482caa1143
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-03-17 03:50:46 +0100

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main _ Entry point .
+ *Return : slway 0 is success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
